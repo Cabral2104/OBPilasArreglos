678adaf0858c4634dc586e74c01456378f4bcc13
Cabral2104 <valdezemilio50@gmail.com>
2023-09-18 19:08:22 -0600

    Agregar archivos de proyecto.

---
+++ Pila.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OBPilasArreglos
+{
+    internal class Pila
+    {
+        int[] items;
+        int contador;
+
+        public Pila(int capacidad)
+        {
+            items = new int[capacidad];
+            contador = 0;
+        }
+
+        public void Apilar(int item)//Agrega un elemento a la pila
+        {
+            if (contador == items.Length)
+                throw new InvalidOperationException("Pila llena");
+
+            items[contador] = item;
+            contador++;
+        }
+
+        public int Desapilar()//Saca el último elemento apilado
+        {
+            if (contador == 0)
+                throw new InvalidOperationException("Pila vacía");
+
+            contador--;
+            return items[contador];
+        }
+
+        public int Cima()// Obtiene el último elemento sin sacarlo
+        {
+            if (contador == 0)
+                throw new InvalidOperationException("Pila vacía");
+
+            return items[contador - 1];
+        }
+
+        public int Tamaño()//Obtiene el número de elementos
+        {
+            return contador;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OBPilasArreglos
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            Pila pila = new Pila(5);
+
+            pila.Apilar(0);
+            pila.Apilar(1);
+            pila.Apilar(2);
+            pila.Apilar(3);
+            pila.Apilar(4);
+
+            Console.WriteLine("Último elemento:" + pila.Cima()); 
+
+            Console.WriteLine("Sacamos el último elemento:" + pila.Desapilar()); 
+
+            Console.WriteLine("Número de elementos restantes: " + pila.Tamaño()); 
+            
+            Console.ReadKey();
+        }
+    }
+}
